a065241bb3af3919c746aca8d66e4ed6640f93a8
angshul <angshul1994@gmail.com>
2019-11-26 20:12:49 -0600

    global month category map

--- app/src/main/java/com/iui/smartbudget/utilities/DataHolder.java
+++ app/src/main/java/com/iui/smartbudget/utilities/DataHolder.java
@@ -2,18 +2,31 @@ package com.iui.smartbudget.utilities;
 
 
 
+import java.time.Month;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
+
 public class DataHolder {
     public static List<Record> records;
+
+    public static List<String> categoriesPriorityList;
+
     public static DataHolder dataHolder = new DataHolder();
     public static HashMap<String, Float> categoryToAvgExpenseMap;
+    public static HashMap<Month, HashMap<String, Float>> monthToCategoryMap;
 
     private DataHolder(){
             records=new ArrayList<>();
+
+            categoriesPriorityList=Arrays.asList(new String[]{"Groceries", "Personal", "Travel", "Dining", "Shopping", "Entertainment"});
             categoryToAvgExpenseMap = new HashMap<>();
+            monthToCategoryMap = new HashMap<>();
+            categoryToAvgExpenseMap = new HashMap<>();
+
+
     }
 
     public static DataHolder getInstance(){
