8d1871ab1cbd736cbc3c5c52de7a822d6ae3de98
Andrew Lam <andrewlam@email.tamu.edu>
2019-11-26 20:31:19 -0600

    Added bucket

--- app/src/main/java/com/iui/smartbudget/ui/budget/BudgetFragment.java
+++ app/src/main/java/com/iui/smartbudget/ui/budget/BudgetFragment.java
@@ -1,5 +1,8 @@
 package com.iui.smartbudget.ui.budget;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,11 +17,45 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.iui.smartbudget.R;
+import com.iui.smartbudget.utilities.Bucket;
 
 public class BudgetFragment extends Fragment {
 
     private BudgetViewModel budgetViewModel;
-    ProgressBar progressBar;
+    private TextView title;
+
+    private ProgressBar entertainmentPB;
+    private TextView entertainmentName;
+    private TextView entertainmentCurrent;
+    private TextView entertainmentTotal;
+
+    private ProgressBar diningPB;
+    private TextView diningName;
+    private TextView diningCurrent;
+    private TextView diningTotal;
+
+
+    private ProgressBar personalPB;
+    private TextView personalName;
+    private TextView personalCurrent;
+    private TextView personalTotal;
+
+    private ProgressBar travelPB;
+    private TextView travelName;
+    private TextView travelCurrent;
+    private TextView travelTotal;
+
+    private ProgressBar groceriesPB;
+    private TextView groceriesName;
+    private TextView groceriesCurrent;
+    private TextView groceriesTotal;
+
+    private ProgressBar shoppingPB;
+    private TextView shoppingName;
+    private TextView shoppingCurrent;
+    private TextView shoppingTotal;
+    private Bucket entertainment;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         budgetViewModel =
@@ -26,19 +63,123 @@ public class BudgetFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_budget, container, false);
         //final TextView textView = root.findViewById(R.id.text_budget);
         //TextView textView1 = root.findViewById(R.id.textViewProgressBar1);
-        //progressBar = root.findViewById(R.id.progressBar);
+        title = root.findViewById(R.id.title);
+        title.setText("The Budget For: " + "December");
+
+        //Entertainment
+        entertainmentPB = root.findViewById(R.id.entertainmentProgressBar);
+        entertainmentName = root.findViewById(R.id.entertainment);
+        entertainmentCurrent = root.findViewById(R.id.entertainmentCurrent);
+        entertainmentTotal = root.findViewById(R.id.entertainmentTotal);
+
+        //Dining
+        diningPB = root.findViewById(R.id.diningProgressBar);
+        diningName = root.findViewById(R.id.dining);
+        diningCurrent = root.findViewById(R.id.diningCurrent);
+        diningTotal = root.findViewById(R.id.diningTotal);
+
+        //Personal
+        personalPB = root.findViewById(R.id.personalProgressBar);
+        personalName = root.findViewById(R.id.personal);
+        personalCurrent = root.findViewById(R.id.personalCurrent);
+        personalTotal = root.findViewById(R.id.personalTotal);
+
+        //Travel
+        travelPB = root.findViewById(R.id.travelProgressBar);
+        travelName = root.findViewById(R.id.travel);
+        travelCurrent = root.findViewById(R.id.travelCurrent);
+        travelTotal = root.findViewById(R.id.travelTotal);
+
+        //Groceries
+        groceriesPB = root.findViewById(R.id.groceriesProgressBar);
+        groceriesName = root.findViewById(R.id.groceries);
+        groceriesCurrent = root.findViewById(R.id.groceriesCurrent);
+        groceriesTotal = root.findViewById(R.id.groceriesTotal);
+
+        //Shopping
+        shoppingPB = root.findViewById(R.id.shoppingProgressBar);
+        shoppingName = root.findViewById(R.id.shopping);
+        shoppingCurrent = root.findViewById(R.id.shoppingCurrent);
+        shoppingTotal = root.findViewById(R.id.shoppingTotal);
+
+        Bucket temp = new Bucket("Entertainment", 111);
+        temp.setName("Entertainment Test");
+        temp.setCapacity(200);
+        temp.setCurrent(25);
+        entertainmentSetters(temp);
+        //entertainmentPB.setProgressTintList(ColorStateList.valueOf(Color.parseColor("#5CB85C")));
+
+
+
+//        Bucket entertainment = new Bucket("Entertainment", 100);
+//        entertainment.setCurrent();
+//        entertainment.setCapacity();
+        //entertainment.setName(String.valueOf(entertainmentName.getText()));
+
+//        entertainmentName.setText(entertainment.getName());
+//        entertainmentCurrent.setText(String.valueOf(entertainment.getCurrent()));
+//        entertainmentTotal.setText(String.valueOf(entertainment.getCapacity()));
+        /*entertaintTV;
+        entertainPB;
+        entertainmentTotal;
+        Bucket entertain = new Bucket("Entertainment", 300);
+        entertain.setCurrent(angshul.give.me.value);
+        entertainmentTotal.setText(entertain.getCurrent() / entertain.getCapacity())
+
+*/
+
+
+//        entertainmentProgressBar = root.findViewById(R.id.entertainmentProgressBar);
+//        entertainmentProgressBar.setMax(150);
+//        entertainmentProgressBar.setProgress(100);
 //        budgetViewModel.getText().observe(this, new Observer<String>() {
 //            @Override
 //            public void onChanged(@Nullable String s) {
 //                textView.setText(s);
 //            }
 //        });
+
         return root;
     }
 
-    public void prograssBar(){
+    public void create(){
+
+    }
+
+    public void entertainmentSetters(Bucket b){
+        entertainmentName.setText(b.getName());
+        entertainmentCurrent.setText(String.valueOf((int)b.getCurrent()));
+        entertainmentTotal.setText(String.valueOf((int)b.getCapacity()));
+        entertainmentPB.setMax((int)b.getCapacity());
+        entertainmentPB.setProgress((int)b.getCurrent());
+        if(b.getCurrent()/b.getCapacity() >= 0.75) { //red
+            entertainmentPB.setProgressTintList(ColorStateList.valueOf(Color.parseColor("#D9534F")));
+            //entertainmentPB.getIndeterminateDrawable().setColorFilter(Color.parseColor("#D9534F"), PorterDuff.Mode.SRC_IN);
+            //entertainmentPB.getProgressDrawable().setColorFilter(Color.parseColor("#D9534F"), PorterDuff.Mode.SRC_IN);
+        }
+        else if(b.getCurrent()/b.getCapacity() < 0.75 && b.getCurrent()/b.getCapacity() >= 0.50){ //yellow
+            entertainmentPB.setProgressTintList(ColorStateList.valueOf(Color.parseColor("#F0AD4E")));
+
+            //entertainmentPB.getIndeterminateDrawable().setColorFilter(Color.parseColor("#F0AD4E"), PorterDuff.Mode.SRC_IN);
+            //entertainmentPB.getProgressDrawable().setColorFilter(Color.parseColor("#F0AD4E"), PorterDuff.Mode.SRC_IN);
+        }
+        else{ //green
+            entertainmentPB.setProgressTintList(ColorStateList.valueOf(Color.parseColor("#D9534F")));
+
+            //entertainmentPB.getIndeterminateDrawable().setColorFilter(Color.parseColor("#5CB85C"), PorterDuff.Mode.SRC_IN);
+            //entertainmentPB.getProgressDrawable().setColorFilter(Color.parseColor("#5CB85C"), PorterDuff.Mode.SRC_IN);
+        }
+
+
+    }
+
+    /*public void (){
         progressBar.setSecondaryProgress(100);
         progressBar.setProgress(10);
         progressBar.setMax(100);
-    }
+    }*/
+
+    /*public void setEntertainmentProgressBar(ProgressBar p, int ){
+        p.
+    }*/
 }
